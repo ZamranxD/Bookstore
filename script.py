796ac2adc0224562d212d4fe39173e32249b0ab9
ZamranxD <zamran.butt.1@gmail.com>
2020-08-12 21:26:35 +0500

    creating a window and adding labels

---
+++ script.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+window = Tk()
+
+label1 = Label(window, text="Title")
+label1.grid(row=0, column=0)
+
+label1 = Label(window, text="Author")
+label1.grid(row=0, column=2)
+
+label1 = Label(window, text="Year")
+label1.grid(row=1, column=0)
+
+label1 = Label(window, text="ISBN")
+label1.grid(row=1, column=2)
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
